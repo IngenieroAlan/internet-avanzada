64a417284bd43dbfd2728c614b64fa37ae96c9b1
IngenieroAlan <brandon.rod182@gmail.com>
2023-11-05 19:44:56 -0700

    Unidad5_Actividad6

---
+++ my-system/resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <h1>Hola <?= $name ?></h1>
+    <h3>Bienvenido a mi pagina</h3>
+    <p>Dolor eiusmod consectetur eu est. Ut ut consectetur
+        eu ullamco cillum aliquip aute exercitation nulla
+        anim anim ut. Irure mollit pariatur est voluptate
+        qui proident consequat dolor pariatur non nisi
+        minim dolor amet. Officia consequat irure ea
+        cupidatat laborum amet officia labore. Irure dolor
+        excepteur sit duis officia occaecat amet voluptate
+        incididunt in. Cillum esse laborum mollit et quis
+        consectetur.</p>
+</body>
+
+</html>
--- my-system/routes/web.php
+++ my-system/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home/{name?}', function ($name="usuario") {
+    return view('home',['name' => $name]);
+});
+Route::get('/saludo/{nombre}/{apellido?}', function (string $nombre, string $apellido = "") {
+    return 'Hola ' . $nombre . ' ' . $apellido;
+})->where(['nombre' => '[A-Za-z]+', 'apellido' => '[A-Za-z]+']);
+
+Route::get('/suma/{numero1}/{numero2}', function (string $numero1, string $numero2 = "") {
+    return 'El resultado de la suma de ' . $numero1 . ' + ' . $numero2 . ' es: ' . $numero1 + $numero2;
+})->where(['numero1' => '[-+]?[0-9]+', 'numero2' => '[-+]?[0-9]+']);
+
+Route::get('/user',[UserController::class,'index']);
